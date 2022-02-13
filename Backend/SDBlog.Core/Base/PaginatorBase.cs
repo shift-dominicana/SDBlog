a73ef4ed81066620e235d835c1f97c8ada3f92d4
Juan Hiciano <juanlhiciano@gmail.com>
2022-02-13 18:05:00 -0400

    #1 - Improve the core Layer

---
+++ Backend/SDBlog.Core/Base/PaginatorBase.cs
@@ -0,0 +1,9 @@
+﻿namespace SDBlog.Core.Base
+{
+    public class PaginatorBase
+    {
+        public int Page { get; set; }
+        public int Take { get; set; }
+        public string OrderBy { get; set; }
+    }
+}
---
+++ Backend/SDBlog.Core/Enums/EntityEstatus.cs
@@ -0,0 +1,9 @@
+﻿
+namespace SDBlog.Core.Enums
+{
+    public static class EntityEstatus
+    {
+        public const string Active = "A";
+        public const string Inactive = "I";
+    }
+}
